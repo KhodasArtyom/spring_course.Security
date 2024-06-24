caf194a93b260424f0f5c3ac1a213872bdaff701
ArtyomKhodas <a.Khodas1601@gmail.com>
2024-06-24 19:01:54 +0300

    ad configures,controller

---
+++ src/main/java/com/artemhodas/spring_security/configuration/MySecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.artemhodas.spring_security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public abstract class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
+
